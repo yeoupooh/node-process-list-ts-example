88f3d02ef750b7b848f366c7ea93e9df440b66c1
Thomas Min <thomas.min@ewoosoft.com>
2021-01-13 12:21:24 +0900

    updated tasks output.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import { snapshot } from "process-list";
 
 const getProcessList = () => {
-  const tasks = snapshot("pid", "name", "path").then((tasks: any) => {
-    console.log(tasks);
-  });
+  const tasks = snapshot("pid", "ppid", "name", "path", "cmdline").then(
+    (tasks: any) => {
+      tasks.map((task: any) => {
+        console.log(`task: ${JSON.stringify(task)}`);
+      });
+    }
+  );
 };
 
 getProcessList();
